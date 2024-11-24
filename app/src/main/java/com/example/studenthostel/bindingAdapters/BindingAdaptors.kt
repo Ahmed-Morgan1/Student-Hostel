f9ab333496250b2299675a302d31f86605c86bdd
Ahmed Fady <faxyahmed6@gmail.com>
2024-11-24 21:56:54 +0200

    - remove navHost activity_auth.xml + add onDestinationChangeListener in MainActivity + fix binding adaptor named type

--- app/src/main/java/com/example/studenthostel/bindingAdapters/BindingAdaptors.kt
+++ app/src/main/java/com/example/studenthostel/bindingAdapters/BindingAdaptors.kt
@@ -3,11 +3,12 @@ package com.example.studenthostel.bindingAdapters
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.example.studenthostel.R
 import com.example.studenthostel.model.Apartment
 
 @BindingAdapter("app:type")
 fun type(view: TextView, type: Apartment.ApartmentStatusType) {
-    view.text = "Apartment for ${type}"
+    view.text = view.context.getString(R.string.apartment_for_type, type)
 }
 
 @BindingAdapter("imgUrl")
