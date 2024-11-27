a34035388cd7988e3ae7d2520858e9ff884a8feb
Ahmed Morgan <elmorg6666@gmail.com>
2024-11-27 22:29:22 +0200

    Eidit UI

--- app/src/main/java/com/example/studenthostel/MainActivity.kt
+++ app/src/main/java/com/example/studenthostel/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.studenthostel
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
@@ -12,11 +13,13 @@ import com.example.studenthostel.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        handleNavigationBottomVisibility()
     }
 
     override fun onResume() {
@@ -30,11 +33,31 @@ class MainActivity : AppCompatActivity() {
             when (navDestination.id) {
                 R.id.homeFragment -> {toggleBottomNav(true)}
                 R.id.signupFragment2 -> {toggleBottomNav(false)}
+                R.id.loginFragment -> {toggleBottomNav(false)}
             }
 
         }
     }
+    private fun handleNavigationBottomVisibility() {
+        // Listen for layout changes
+        binding.root.viewTreeObserver.addOnGlobalLayoutListener {
+            val rect = Rect()
+            binding.root.getWindowVisibleDisplayFrame(rect)
 
+            // Get screen height and compare with visible window height
+            val screenHeight = binding.root.rootView.height
+            val keypadHeight = screenHeight - rect.bottom
+
+            if (keypadHeight > screenHeight * 0.15) {
+                // If more than 15% of the screen height is covered, the keyboard is open
+                binding.navigationHome.visibility = View.GONE
+            } else {
+                // Keyboard is closed
+                this.handleDesignationNavyHost()
+            }
+
+        }
+    }
     private fun toggleBottomNav(show: Boolean) {
         binding.navigationHome.visibility = if (show) View.VISIBLE else View.GONE
     }
